3e85d5f9e12abe75e263d0ff128fff7564e8c16f
Michael Tolly <onyxite@gmail.com>
2014-01-16 12:26:15 -0600

    cleaned up imports

--- Main.hs
+++ Main.hs
@@ -1,47 +1,48 @@
-module Main where
+module Main (main) where
 
-import Paths_ogg2mogg
-import System.Environment
+import Data.Bits (shiftL)
+import Data.Word (Word32)
+import System.Environment (getArgs, getProgName)
 import System.Info (os)
-import System.FilePath
-import System.Directory
-  (copyFile, setCurrentDirectory, getCurrentDirectory, createDirectory)
-import System.IO
+import qualified System.IO as IO
+
+import qualified Data.ByteString as B
+import qualified System.Directory as Dir
+import System.FilePath ((</>))
 import System.IO.Temp (withSystemTempDirectory)
 import System.Process (readProcess)
-import Data.Bits (shiftL)
-import qualified Data.ByteString as B
-import Data.Word (Word32)
+
+import Paths_ogg2mogg (getDataFileName)
 
 main :: IO ()
 main = do
   argv <- getArgs
   case argv of
     [x, y] -> run x y
-    [x] -> run x $ x ++ ".mogg"
-    _ -> do
+    [x]    -> run x $ x ++ ".mogg"
+    _      -> do
       prog <- getProgName
-      hPutStrLn stderr $ "Usage: " ++ prog ++ " in.ogg out.mogg"
+      IO.hPutStrLn IO.stderr $ "Usage: " ++ prog ++ " in.ogg out.mogg"
 
 copyDataTo :: FilePath -> FilePath -> IO FilePath
 copyDataTo tmp f = do
   orig <- getDataFileName f
   let new = tmp </> f
-  copyFile orig new
+  Dir.copyFile orig new
   return new
 
 run :: FilePath -> FilePath -> IO ()
 run oggRel moggRel = do
-  pwd <- getCurrentDirectory
+  pwd <- Dir.getCurrentDirectory
   let ogg  = pwd </> oggRel
       mogg = pwd </> moggRel
   withSystemTempDirectory "ogg2mogg" $ \tmp -> do
-    setCurrentDirectory tmp
+    Dir.setCurrentDirectory tmp
     magma <- copyDataTo tmp "MagmaCompiler.exe"
     let ogg' = tmp </> "audio.ogg"
-    copyFile ogg ogg'
+    Dir.copyFile ogg ogg'
     rbproj <- copyDataTo tmp "hellskitchen.rbproj"
-    createDirectory $ tmp </> "gen"
+    Dir.createDirectory $ tmp </> "gen"
     mapM_ (copyDataTo tmp)
       [ "notes.mid", "cover.bmp", "silence.wav", "oggenc.exe"
       , "gen/main_pc.hdr", "gen/main_pc_0.ark" ]
@@ -49,16 +50,16 @@ run oggRel moggRel = do
     _ <- if os == "mingw"
       then readProcess magma [rbproj, rba] ""
       else readProcess "wine" [magma, rbproj, rba] ""
-    withBinaryFile rba ReadMode $ \hrba -> do
-      hSeek hrba AbsoluteSeek $ 4 + (4 * 3)
+    IO.withBinaryFile rba IO.ReadMode $ \hrba -> do
+      IO.hSeek hrba IO.AbsoluteSeek $ 4 + (4 * 3)
       moggOffset <- hReadWord32le hrba
-      hSeek hrba AbsoluteSeek $ 4 + (4 * 10)
+      IO.hSeek hrba IO.AbsoluteSeek $ 4 + (4 * 10)
       moggLength <- hReadWord32le hrba
-      hSeek hrba AbsoluteSeek $ fromIntegral moggOffset
+      IO.hSeek hrba IO.AbsoluteSeek $ fromIntegral moggOffset
       moggData <- B.hGet hrba $ fromIntegral moggLength
       B.writeFile mogg moggData
 
-hReadWord32le :: Handle -> IO Word32
+hReadWord32le :: IO.Handle -> IO Word32
 hReadWord32le h = do
   [a, b, c, d] <- fmap (map fromIntegral . B.unpack) $ B.hGet h 4
   return $ a + (b `shiftL` 8) + (c `shiftL` 16) + (d `shiftL` 24)
